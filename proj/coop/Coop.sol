9907943aa7d758a475dc6d4a561700215be00822
xshrim <xshrim@gmail.com>
2019-07-04 14:11:17 +0800

    update

--- proj/coop/Coop.sol
+++ proj/coop/Coop.sol
@@ -191,6 +191,23 @@ contract Coop {
         return string(bytesStringTrimmed);
     }
     */
+    
+    /* IPFS地址转bytes32数组(js代码)
+    function ipfsHashToBytes32(ipfs_hash) {
+        var h = bs58.decode(ipfs_hash).toString("hex").replace(/^1220/, "");
+        if (h.length != 64) {
+            console.log('invalid ipfs format', ipfs_hash, h);
+            return null;
+        }
+        return '0x' + h;
+    }
+
+    function bytes32ToIPFSHash(hash_hex) {
+        //console.log('bytes32ToIPFSHash starts with hash_buffer', hash_hex.replace(/^0x/, ''));
+        var buf = new Buffer(hash_hex.replace(/^0x/, "1220"), "hex")
+        return bs58.encode(buf)
+    }
+    */
 
     /* 功能函数
     function StringToBytesVer1(string memory source) public pure returns (bytes result) {
